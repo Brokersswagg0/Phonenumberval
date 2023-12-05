12097c883cd0b3b8e93d56256480e8c3c530f04a
Brokersswagg0 <135990498+Brokersswagg0@users.noreply.github.com>
2023-12-04 19:02:41 -0700

    Add files via upload

---
+++ 2.setup.bat
@@ -0,0 +1,15 @@
+@echo off
+python -m pip install colorama
+python -m pip install requests
+python -m pip install fore
+python -m pip install six
+python -m pip install bs4
+python -m pip install beautifulsoup
+python -m pip install future
+py -m pip install colorama
+py -m pip install requests
+py -m pip install fore
+py -m pip install six
+py -m pip install bs4
+py -m pip install beautifulsoup
+py -m pip install future
